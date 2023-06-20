e557d068c6e2a53b10df865e35ed1b6f16ab3ed4
agentgrey <himadrinayak2702@gmail.com>
2023-06-20 22:13:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+/* ------------ IMPORT COMPONENTS ------------ */
+import Navbar from "./Components/Navbar/navbar";
+import Daily from "./Pages/DailyView/dailyView";
+import Weekly from "./Pages/WeeklyView/weeklyView";
+/* ------------ IMPORT ROUTER ------------ */
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+
 
 function App() {
+
+  const router = createBrowserRouter([
+     {path : '/' , element : <Navbar /> , 
+      children : [
+        {path : '/' , element : <Daily />},
+        {path: '/weekly', element: <Weekly />}
+      ]}
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router} />
     </div>
   );
 }
